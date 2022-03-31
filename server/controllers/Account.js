a419140891a1743ec7ee27fa404d83905c1e6c16
Matthew <sorianomatthew@yahoo.com>
2022-03-30 23:14:11 -0400

    domomaker D finished

--- server/controllers/Account.js
+++ server/controllers/Account.js
@@ -6,10 +6,6 @@ const loginPage = (req, res) => {
   res.render('login', { csrfToken: req.csrfToken() });
 };
 
-const signupPage = (req, res) => {
-  res.render('signup', { csrfToken: req.csrfToken() });
-};
-
 const logout = (req, res) => {
   req.session.destroy();
   res.redirect('/');
@@ -62,10 +58,12 @@ const signup = async (req, res) => {
   }
 };
 
+const getToken = (req, res) => res.json({ csrfToken: req.csrfToken() });
+
 module.exports = {
   loginPage,
-  signupPage,
   login,
   logout,
   signup,
+  getToken,
 };
